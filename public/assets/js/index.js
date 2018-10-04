a2068aef9261fe3ba8b7962b3f9853b65f624c70
Raul <raultablada@yahoo.com>
2018-10-04 18:59:33 -0400

    almost done

---
+++ public/assets/js/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    var articleContainer = $(".article-container");
+    $(document).on("click", ".btn.save", handleArticleSave);
+    $(document).on("click", ".scrape-new", handleArticleScrape);
+
+    initPage();
+
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=false")
+            .then(function(data) {
+                if (data && data.length) {
+                    renderArticles(data);
+                }
+                else {
+                    renderEmpty();
+                }
+            });
+    }
+
+    function renderArticles(articles) {
+        var articlePanels = [];
+
+        for (var i = 0; i < article.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+    }
+
+    function createPanel(article) {
+        var panel = 
+            $(["<div class='panel panel-default'>",
+                "<div class='panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class='btn btn-success save'>",
+                "Save Article",
+                "</a>",
+                "</h3>",
+                "</div>",
+                "</div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"
+            ].join(""));
+        panel.data("_id", article._id);
+
+        return panel;
+    }
+
+    function renderEmpty() {
+        var emptyAlert = 
+            $(["<div class='alert alert-warning text-center'>",
+                "<h4>Uh Oh, Looks like we don;t have any new articles.</h4>",
+                "</div>",
+                "<div class='panel panel-default'>",
+                "<div class='panel-heading text-center'>",
+                "<h3>What Would You Like To Do?</h3>",
+                "</div>",
+                "<div class='panel-body text-center'>",
+                "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+                "<h4><a class='/saved'>Go to Saved Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join(""));
+        articleContainer.append(emptyAlert);
+    }
+
+    function handleArticleSave() {
+        var articleveToSave = $(this).parents(".panel").data();
+    }
+})
