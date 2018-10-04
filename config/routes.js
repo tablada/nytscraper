2f7f337eb83ee1aee0ba983a92667ab3b338f4c5
Raul <raultablada@yahoo.com>
2018-10-04 14:42:31 -0400

    controller files

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,13 @@
+// Server Routes
+// =============
+
+// Bring in the Scrape function from our scripts directory
+var scrape = require("../scripts/scrape");
+
+// Bring headlines and notes from the controller
+var headlinesController = require("../controllers/headline");
+var notesController = require("../controllers/note");
+
 module.exports = function(router) {
     //This route renders the homepage
     router.get("/", function(req, res) {
